aa6f576ddea7d28175e74b9a4b46769ec69a20f5
mbp <nicolasmonzon0@gmail.com>
2019-04-01 23:52:27 -0300

    Funcion Flecha

---
+++ 02-Fundamentos/flecha.js
@@ -0,0 +1,25 @@
+//1
+// function sumar(a, b) {
+//     return a + b;
+// }
+
+//2
+// let sumar = (a, b) => {
+//     return a + b;
+// }
+
+//3
+//let sumar = (a, b) => a + b;
+//console.log(sumar(10, 20));
+//1 == 2 == 3
+
+//Segunda manera
+// let saludo = () => "Hola Mundo"
+// console.log(saludo());
+
+//Tercer manera
+// function saludar(nombre) {
+//     return `Hola ${nombre}`;
+// }
+let saludar = (nombre) => `Hola ${nombre}`
+console.log(saludar('Nico'));
